bdd6bf52bed755e7ac1208f81575cf32060ae541
Dee3w <joydana>
2023-06-13 12:37:47 +0300

    changes on index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
   // Write your code here!
-}
+  let response;
+  if (feet <= 400 && feet > 0)  {
+    response = 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    response = 'No can do.';
+  }
+    return response;
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+}
+console.log(ternaryCheckCity('NYC'));
+console.log(ternaryCheckCity('LA')); 
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let response;
+  switch (tip) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
